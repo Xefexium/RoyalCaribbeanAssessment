5f1715eeb96ec6133d0fb157cab7e618644e23c4
= <atorres1929@gmail.com>
2024-01-21 14:13:14 -0500

    Added response log to __request

--- timezoneDBAPI.py
+++ timezoneDBAPI.py
@@ -14,6 +14,7 @@ class TimezoneDBAPI:
         logging.info('Requesting %s' % endpoint)
         response = requests.get(self.BASE_URL + endpoint, params=params)
 
+        logging.info('Response status code: %s' % response.status_code)
         if response.status_code == 200:
             return response.json()
         else:
